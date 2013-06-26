a9c455d2bda63a2511afd62a16221654d79e269e
Joey Adams <joeyadams3.14159@gmail.com>
2013-06-26 03:37:54 -0400

    iocp.c, iocp.h: add structures and functions for allocating task objects to start IO using C

--- cbits/iocp.c
+++ cbits/iocp.c
@@ -1,5 +1,5 @@
-#include <errno.h>
-#include <windows.h>
+#include "iocp.h"
+#include <assert.h>
 
 // Original code from Win32 package (errors.c)
 LPWSTR iocp_getErrorMessage(DWORD err)
@@ -19,4 +19,107 @@ LPWSTR iocp_getErrorMessage(DWORD err)
     if (res == 0)
         return NULL;
     return what;
+}
+
+static void *iocp_alloc(size_t size)
+{
+    assert(size >= sizeof(Overlapped));
+    void *ol = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size);
+    if (ol == NULL)
+        SetLastError(ERROR_NOT_ENOUGH_MEMORY);
+    return ol;
+}
+
+void iocp_free(void *ol)
+{
+    if (ol != NULL)
+        HeapFree(GetProcessHeap(), 0, ol);
+}
+
+void *iocp_alloc_start(size_t size, StartCallback callback)
+{
+    Overlapped *ol = iocp_alloc(size);
+    if (ol != NULL) {
+        ol->tag = O_START;
+        ol->start.callback = callback;
+    }
+    return ol;
+}
+
+void *iocp_alloc_cancel(HANDLE handle)
+{
+    Overlapped *ol = iocp_alloc(sizeof(Overlapped));
+    if (ol != NULL) {
+        ol->tag = O_CANCEL;
+        ol->cancel.handle = handle;
+    }
+    return ol;
+}
+
+// Start an allocated I/O request.  Return TRUE if a completion is expected to
+// be delivered when the I/O is done, or FALSE on failure where no completion
+// is expected.
+BOOL iocp_start(Overlapped *ol)
+{
+    assert(ol->tag == O_START);
+    StartCallback start_callback = ol->start.callback;
+    HsStablePtr signal_callback = ol->start.signal_callback;
+
+    // Morph the Overlapped into an O_SIGNAL so when it appears in the
+    // completion port, the manager will know to treat it as completed I/O
+    // instead of another start request.
+    ol->tag = O_SIGNAL;
+    ol->signal.callback = signal_callback;
+
+    return start_callback(ol);
+}
+
+typedef struct {
+    Overlapped base;
+    HANDLE handle;
+    LPVOID buffer;
+    DWORD bytesToRead;
+} ol_ReadFile;
+
+static BOOL cb_ReadFile(void *ol)
+{
+    ol_ReadFile *ctx = ol;
+    BOOL ok = ReadFile(ctx->handle, ctx->buffer, ctx->bytesToRead, NULL, ol);
+    return (ok || GetLastError() == ERROR_IO_PENDING);
+}
+
+Overlapped *iocp_alloc_ReadFile(HANDLE handle, LPVOID buffer, DWORD bytesToRead)
+{
+    ol_ReadFile *ctx = iocp_alloc_start(sizeof(*ctx), cb_ReadFile);
+    if (ctx != NULL) {
+        ctx->handle = handle;
+        ctx->buffer = buffer;
+        ctx->bytesToRead = bytesToRead;
+    }
+    return (Overlapped*) ctx;
+}
+
+typedef struct {
+    Overlapped base;
+    HANDLE handle;
+    LPCVOID buffer;
+    DWORD bytesToWrite;
+} ol_WriteFile;
+
+static BOOL cb_WriteFile(void *ol)
+{
+    ol_WriteFile *ctx = ol;
+    BOOL ok = WriteFile(ctx->handle, ctx->buffer, ctx->bytesToWrite, NULL, ol);
+    return (ok || GetLastError() == ERROR_IO_PENDING);
+}
+
+Overlapped *iocp_alloc_WriteFile(HANDLE handle, LPCVOID buffer, DWORD bytesToWrite)
+{
+    ol_WriteFile *ctx = iocp_alloc_start(sizeof(*ctx), cb_WriteFile);
+    if (ctx != NULL) {
+        ctx->handle = handle;
+        ctx->buffer = buffer;
+        ctx->bytesToWrite = bytesToWrite;
+    }
+    return (Overlapped*) ctx;
 }
--- include/iocp.h
+++ include/iocp.h
@@ -5,3 +5,57 @@ typedef struct _OverlappedRec {
     OVERLAPPED  ol;
     HsStablePtr sptr;
 } OverlappedRec;
+
+// Tells an I/O manager thread what to do when the Overlapped appears on the
+// completion port.
+typedef enum _OverlappedTag {
+    O_START,
+    O_SIGNAL,
+    O_CANCEL,
+} OverlappedTag;
+
+/*
+ * Before an I/O request is started, an Overlapped is allocated with
+ * information on how to start it (namely, a callback along with
+ * operation-specific context, supplied by "subclassing" Overlapped).
+ * The reason we don't just call e.g. ReadFile on the spot is because
+ * CancelIo is sensitive to what threads operations are started from.  When
+ * CancelIoEx is not available, we sometimes need to send the task of starting
+ * I/O to another thread so we can cancel it without canceling other operations
+ * on the same handle.  The reason the start callback has to be written in C
+ * is so we can do this under the non-threaded RTS, where it is dangerous to
+ * call into Haskell from forked OS threads.
+ */
+typedef BOOL (*StartCallback)(void *overlapped);
+
+typedef struct _Overlapped {
+    OVERLAPPED base;
+    OverlappedTag tag;
+    union {
+        // O_START
+        struct {
+            // Callback that starts the overlapped I/O, returning TRUE if a
+            // completion is expected to appear when the I/O is done.
+            StartCallback callback;
+
+            // Haskell function called when the I/O completes.
+            // This is initialized by the I/O manager.
+            HsStablePtr signal_callback;
+        } start;
+
+        // O_SIGNAL
+        struct {
+            // Called when this overlapped is dequeued from the completion port.
+            HsStablePtr callback;
+        } signal;
+
+        // O_CANCEL
+        //    Call CancelIo on the given handle, then free this Overlapped.
+        //    CancelIo cancels pending I/O issued by the current thread on
+        //    the given handle, allowing us to cancel individual operations
+        //    in the absence of CancelIoEx.
+        struct {
+            HANDLE handle;
+        } cancel;
+    };
+} Overlapped;
